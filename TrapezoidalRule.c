d63616074ffd55d6451490ad6646150019b63b70
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-07-07 10:49:51 +0530

    Update TrapezoidalRule.c

--- TrapezoidalRule.c
+++ TrapezoidalRule.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
-float fun(float,int,float ,float );
+float fun(float,int);
 int main()
 {
 	float a,x,b,h;
@@ -13,37 +13,19 @@ int main()
 	scanf("%f",&b);
 	printf("Enter the no of sub intervals : ");
 	scanf("%d",&n);
-	float k[n],y[n];
-	printf("Enter data points x :");
-	for(i=0;i<n;i++)
-	{
-		scanf("%f",&k[i]);
-	}
-	printf("Enter data points y :");
-	for(i=0;i<n;i++)
-	{
-		scanf("%f",&y[i]);
-	}
 	float sum = 0;
 	h = (b-a)/n;
 	for(i=1;i<=n-1;i++)
 	{
 		x = a + (i*h);
-		sum = sum + 2*fun(x,n,k[n],y[n]);
+		sum = sum + 2*fun(x,n);
 	}
-	sum = sum + (fun(a,n,k[n],y[n]) + fun(b,n,k[n],y[n]));
+	sum = sum + (fun(a,n) + fun(b,n));
 	sum = (h*sum)/2;
 	printf("The value of integral = %f\n",sum);
 	return 0;
 }
-float fun(float a,int n,float k[n],float y[n])
+float fun(float a,int n)
 {
-	int i;
-	for(i=0;i<n;i++)
-	{
-		if(a==k[i])
-		{
-			return y[i];
-		}
-	}
+	return sin(a);
 }
